f5651afbdd885745e36fc6833992bb2946d22e5b
chonlathit <ke.chonlathit_st@tni.ac.th>
2017-02-03 02:57:55 +0000

    Commit 4 - generate_list v2

--- src/generate_list.py
+++ src/generate_list.py
@@ -1,6 +1,10 @@
 import random
 def generate_list():
-    alist = [x for x in range(random.randint(-10,10))]
+    
+    alist = [x for x in range(random.randint(1,100))]
+    
+    assert (len(alist)!=0),"alist is empty"    
+    
     return alist
     
 """
